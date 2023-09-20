2dc4cbd1a91848fa3c92345fc2a3512333a45039
Eric! <eric@ericmikkelsen.com>
2023-09-19 20:37:02 -0700

    feat: gets basic stuff setup

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+const sortedColors = require('./_data/sortedColors.json');
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addCollection("colors", function(collectionApi) {
+        return Object.keys(sortedColors).map(color => {
+            return {
+                name:color,
+                colors: sortedColors[color],
+            }
+        })
+    });
+}
