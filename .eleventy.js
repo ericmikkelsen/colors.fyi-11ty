96ef461c06e53899b7ecacedf9f1c74f95d680c4
Eric! <eric@ericmikkelsen.com>
2023-09-21 22:33:21 -0700

    feat: adds a few css updates for full width color bars

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
 const sortedColors = require('./_data/sortedColors.json');
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addCollection("colors", function(collectionApi) {
         return Object.keys(sortedColors).map(color => {
             return {
